57c5f90c463a07a1859ebc8e68904247d83b79f0
CEN Xiyue <xiuye.25@intl.zju.edu.cn>
2026-05-09 13:50:41 +0800

    Revised count_codons.py

--- Practical7/count_codons.py
+++ Practical7/count_codons.py
@@ -40,8 +40,44 @@ print("Counts of in-frame codons upstream of", stop)
 for codon in counts:
     print(codon, counts[codon])
 
-# draw a pie chart
-plt.pie(counts.values(), labels=counts.keys(), autopct='%1.1f%%')
-plt.title(f"{stop} distrubution of all in-frame codons")
-plt.savefig(f"pie_{stop}.png")
+
+# draw a big pie chart
+# Combine the codons whose porpotions are <2% into the group "others" to make the pie chart clearer
+total = sum(counts.values())
+main_counts = {}
+other_codons = []
+
+for codon, cnt in counts.items():
+    if (cnt / total) * 100 < 2:
+        other_codons.append(codon)
+    else:
+        main_counts[codon] = cnt
+
+if other_codons:
+    #split the lines other codons every 10 codons
+    codon_lines = []
+    for i in range(0, len(other_codons), 10):
+        codon_lines.append(", ".join(other_codons[i:i+10]))
+    codon_str = ",\n".join(codon_lines)
+    # label other codons
+    label = f"Others\n(contributions <2%):\n{codon_str}"
+    main_counts[label] = sum(counts[c] for c in other_codons)
+
+plt.figure(figsize=(16, 16), dpi=150)
+
+wedges, texts, autotexts = plt.pie(
+    main_counts.values(),
+    labels=main_counts.keys(),
+    autopct='%1.1f%%',
+    pctdistance=0.7,
+    labeldistance=1.05,
+    startangle=90,
+    textprops={'fontsize': 9},
+    rotatelabels=False  
+)
+plt.setp(autotexts, size=9, weight="bold", color="white")
+
+plt.title(f"{stop} distribution of all in-frame codons", fontsize=16, pad=30)
+plt.tight_layout()
+plt.savefig(f"pie_{stop}.png", bbox_inches='tight')
 plt.close()
